6642ae83a7c0ab11c9165c00add81b62246cb0c4
Ildar <bakirovildar2011@mail.ru>
2022-01-18 10:45:11 +0500

    refactoring

--- src/Components/Header/header.Icon/HeaderCart/header.cart.js
+++ src/Components/Header/header.Icon/HeaderCart/header.cart.js
@@ -4,9 +4,39 @@ import classes from './HeaderCart.module.css'
 const HeaderCart = props => {
     return (
         <div className={classes.HeaderCart}>
-            <div>
+            <div className={classes.wrapperCart}>
 
-                <h1>Cart</h1>
+                <div className={classes.productCart}>
+                    <div className={classes.Cart}>
+                        <img src="" alt=""/>
+                        <div>
+                            <a href="">White Shirt With Pleat Detail Back</a>
+                            <p>1 x $19.00</p>
+                        </div>
+                    </div>
+
+                    <div className={classes.Cart}>
+                        <img src="" alt=""/>
+                        <div>
+                            <a href="">White Shirt With Pleat Detail Back</a>
+                            <p>1 x $19.00</p>
+                        </div>
+                    </div>
+
+                    <div className={classes.Cart}>
+                        <img src="" alt=""/>
+                        <div>
+                            <a href="">White Shirt With Pleat Detail Back</a>
+                            <p>1 x $19.00</p>
+                        </div>
+                    </div>
+
+                </div>
+                <div className={classes.totalCart}><p>total:$75</p></div>
+                <div className={classes.cartFooter}>
+                    <button>view cart</button>
+                    <button>checkout</button>
+                </div>
 
             </div>
 
